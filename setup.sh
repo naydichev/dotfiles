a524c98c7e5c7b34cbc4ddb12a4c686a0ed6afc7
Julian Naydichev <naydiche@amazon.com>
2016-06-21 09:24:39 -0700

    fix a bug in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-OS=`uname`;
+OS=$(uname);
 BPATH="$HOME/.bashrc.";
+BASHRC=".bashrc.";
 
-for file in `ls $HOME/.bashrc.*`; do
-    if [ $file != "${BPATH}load" -a $file != "${BPATH}${OS}" ]; then
-        unlink $file;
-    fi
+EXCEPTION=("${BASHRC}amazon", "${BASHRC}${OS}");
+
+EX_STR=""
+for name in "${EXCEPTION[@]}"; do
+    EX_STR="$EX_STR -and -not -name ${name/,/}";
 done
+
+FILES=$(find $HOME -maxdepth 1 -type l -name "${BASHRC}*" $EX_STR);
+echo "Clearing non-essential ${BASHRC/%./} files: $FILES"
+
+if [ -n "$FILES" ]; then
+    echo $FILES | xargs unlink
+fi
